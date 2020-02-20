8d131e748d17b52af2bd62f9966fecf7db79d81b
Justin Gonzalez <jfgonzalez99@gmail.com>
2020-02-19 23:52:38 -0500

    Created CompleteGraph and Vertex classes

---
+++ CompleteGraph.java
@@ -0,0 +1,21 @@
+/**
+ * CompleteGraph constructs a complete weighted graph on n vertices with random 
+ * edge weights depending on the number of dimensions the user inputs.
+ * 
+ * @author Justin Gonzalez
+ * @author Carlos Robles
+ */
+public class CompleteGraph {
+    int dimension;
+    int n;
+
+    // Complete Graph Class Constructor
+    public CompleteGraph(int d, int m) {
+        dimension = d;
+        n = m;
+    }
+
+    public void name() {
+        
+    }
+}
